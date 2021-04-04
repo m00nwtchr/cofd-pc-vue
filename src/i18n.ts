14d59f91119f4937801b9146085b82ddd02a0e35
m00nwtchr <m00nwtchr@duck.com>
2021-04-05 01:28:57 +0200

    automatic language detection

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,9 +3,15 @@ import messages from "@intlify/vite-plugin-vue-i18n/messages";
 
 const env = window["process.env" as any] as any || {};
 
+function getLang() {
+	if (navigator.languages != undefined)
+		return navigator.languages[0];
+	return navigator.language;
+}
+
 export default createI18n({
 	legacy: false,
-	locale: env.VUE_APP_I18N_LOCALE || "en",
+	locale: env.VUE_APP_I18N_LOCALE || getLang(),
 	fallbackLocale: "en",
 	globalInjection: true,
 	messages
